248455ac4f4c6e0132e645596aaecb5a541565f6
Nick Morse <ndmorse45@gmail.com>
2026-02-05 20:06:32 -0500

    Update Database.sql

--- Database/Database.sql
+++ Database/Database.sql
@@ -8,6 +8,7 @@ email VARCHAR(100) NOT NULL UNIQUE,
 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Switched to discrete rating ('disliked', 'liked', 'loved') & rating value for algorithm
 CREATE TABLE ratings (
 id INT AUTO_INCREMENT PRIMARY KEY,
 rating_name VARCHAR(20) NOT NULL UNIQUE,
@@ -19,6 +20,7 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 genre_name VARCHAR(100) NOT NULL UNIQUE
 );
 
+-- Lists every user's preferred genres (similar to pintrest's 'hobbies & interests')
 CREATE TABLE users_preferred_genres (
 user_id INT NOT NULL,
 genre_id INT NOT NULL,
@@ -27,6 +29,7 @@ FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
 );
 
+-- Holds all data on each game
 CREATE TABLE games (
 id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(100) NOT NULL,
@@ -36,7 +39,6 @@ avg_rating DECIMAL(3,2) NULL,
 total_ratings INT UNSIGNED NULL,
 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
 CREATE INDEX idx_game_title ON games(title);
 CREATE INDEX idx_game_avg_rating ON games(avg_rating);
 
@@ -48,7 +50,7 @@ FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
 FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
 );
 
--- Junction table to hold all user game ownership data
+-- Junction table to hold all user game data
 CREATE TABLE users_games (
 user_id INT NOT NULL,
 game_id INT NOT NULL,
